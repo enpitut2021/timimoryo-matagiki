67ace87fac09d682a11bf2a13a97f9893bd33ce2
takumah1234 <hidaka@iplab.cs.tsukubal.ac.jp>
2021-07-30 09:30:20 +0900

    UIが表示できるか確認

--- src/app.js
+++ src/app.js
@@ -34,6 +34,66 @@ app.command('/matagiki', async ({command, ack, say, context}) => {
     }
 });
 
+app.command('/ticket', async ({ ack, body, client }) => {
+    // コマンドのリクエストを確認
+    await ack();
+
+    try {
+        const result = await client.views.open({
+        // 適切な trigger_id を受け取ってから 3 秒以内に渡す
+        trigger_id: body.trigger_id,
+        // view の値をペイロードに含む
+        view: {
+            type: 'modal',
+            // callback_id が view を特定するための識別子
+            callback_id: 'view_1',
+            title: {
+            type: 'plain_text',
+            text: 'Modal title'
+            },
+            blocks: [
+            {
+                type: 'section',
+                text: {
+                type: 'mrkdwn',
+                text: 'Welcome to a modal with _blocks_'
+                },
+                accessory: {
+                type: 'button',
+                text: {
+                    type: 'plain_text',
+                    text: 'Click me!'
+                },
+                action_id: 'button_abc'
+                }
+            },
+            {
+                type: 'input',
+                block_id: 'input_c',
+                label: {
+                type: 'plain_text',
+                text: 'What are your hopes and dreams?'
+                },
+                element: {
+                type: 'plain_text_input',
+                action_id: 'dreamy_input',
+                multiline: true
+                }
+            }
+            ],
+            submit: {
+            type: 'plain_text',
+            text: 'Submit'
+            }
+        }
+        });
+        console.log(result);
+    }
+    catch (error) {
+        console.error(error);
+    }
+});
+
 (async () => {
     // Start your app
     await app.start(process.env.PORT || 3000);
