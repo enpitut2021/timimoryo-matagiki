0936610682ea835471e3285485751ef135ababf5
thistle519 <shizuru519@gmail.com>
2021-07-30 11:12:06 +0900

    fix:余計なものを消して、コマンドをmatagikiに変更

--- src/app.js
+++ src/app.js
@@ -6,35 +6,7 @@ const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-// Listens to incoming messages that contain "hello"
-// app.message('', async ({message, say}) => {
-//     // say() sends a message to the channel where the event was triggered
-//     await say(`${message.text}`);
-// });
-
-// この echo コマンドは ただ、その引数を（やまびこのように）おうむ返しする
-app.command('/matagiki', async ({command, ack, say, context}) => {
-    // コマンドリクエストを確認
-    await ack();
-
-    await say(`「${command.text}」> 質問受け付けました．しばらくお待ちください．`);
-
-    const channelId = 'C029QSVP30C'
-
-    try {
-        // トークンを用いて chat.scheduleMessage 関数を呼び出す
-        const result = await app.client.chat.postMessage({
-            // アプリの初期化に用いたトークンを `context` オブジェクトに保存
-            token: context.botToken,
-            channel: channelId,
-            text: `<@${command.user_name}>「${command.text}」`
-        });
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-app.command('/ticket', async ({ ack, body, client }) => {
+app.command('/matagiki', async ({ ack, body, client }) => {
     // コマンドのリクエストを確認
     await ack();
 
